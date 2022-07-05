290e05372b4acaad8f52578f8dad81ec92f24034
BuildTools <unconfigured@null.spigotmc.org>
2022-07-05 21:39:26 +0400

    Removed redundant ENV imports

--- saba.py
+++ saba.py
@@ -12,18 +12,11 @@ POSTS_IMAGES = os.path.join('static', 'post_images')
 
 # Initialize additional config
 app.secret_key = FLASK_SECRET_KEY
-app.config['MYSQL_HOST'] = MYSQL_HOST
-app.config['MYSQL_USER'] = MYSQL_USER
-app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
-app.config['MYSQL_DB'] = MYSQL_DB
-app.config['MYSQL_CURSORCLASS'] = MYSQL_CURSORCLASS
 app.config['UPLOAD_FOLDER'] = POSTS_IMAGES
 app.config['MONGODB_NAME'] = MONGODB_NAME
 app.config['MONGODB_HOST'] = MONGODB_HOST
 app.config['MONGODB_PORT'] = MONGODB_PORT
 app.config['MONGODB_USER'] = MONGODB_USER
-app.config['RECAPTCHA_PUBLIC_KEY'] = RECAPTCHA_PUBLIC_KEY
-app.config['RECAPTCHA_PRIVATE_KEY'] = RECAPTCHA_PRIVATE_KEY
 
 # Routes
 @app.route('/', methods=['GET', 'POST'])
